785f814690e1cb3e040bdde6a004f2c0d02e8c25
Vagner Filho <vagner.filhopro@gmail.com>
2022-04-07 20:03:04 -0400

    setup: vue router / use libs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
